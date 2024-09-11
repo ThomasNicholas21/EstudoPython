76ba18e4e4c726bdbb9786b25e86cf94a30e456d
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-09-10 22:49:48 -0300

    Funcoes alteradas

--- exercicios/estrutura_de_dados/lista_tarefas/lista_tarefas.py
+++ exercicios/estrutura_de_dados/lista_tarefas/lista_tarefas.py
@@ -1,42 +1,73 @@
 # Lista de Tarefas
 # Definição: realizar a listagem, desfaz uma ação e refazaz uma ação 
-
+import os
 
 class lista_tarefas:
-    def inserir_tarefa(self, valor, lista_tarefa):
-        lista_tarefa.append(valor)
+    def __init__(self, lista):
+        self.lista = lista
+    
+    def inserir_tarefa(self, valor):
+        self.lista.append(valor)
 
-    def apaga_tarefa(self, valor, lista_tarefa, lista_lixeira):
-        lista_lixeira.append(valor)
-        lista_tarefa.remove(valor)
+    def apaga_tarefa(self, lista_lixeira):
+        if self.lista:
+            valor = self.lista.pop()
+            lista_lixeira.append(valor)
 
-    def refaz_tarefa(self, valor, lista_tarefa, lista_lixeira):
-        for item in lista_lixeira:
-            if item == valor:
-                lista_tarefa.append(item)
+    def refaz_tarefa(self, lista_lixeira):
+        if lista_lixeira:
+            valor = lista_lixeira.pop()
+            self.lista.append(valor)
 
     def __str__(self):
-        return f'{self.__name__.__class__}{'\n'.join([item for item in self.lista])}'
+        return f'{self.__class__.__name__}:\n{'\n'.join([f'{item}' for item in self.lista])}'
 
-def listar(lista_tarefas):
-    return print(lista_tarefas)
+def listar(lista_tarefa):
+    tarefas = lista_tarefas(lista_tarefa)
+    print(tarefas)
+
+def inserir_tarefa(lista_tarefa, tarefas):
+    tarefa = lista_tarefas(lista_tarefa)
+    tarefa.inserir_tarefa(tarefas)
     
+def desfazer(lista_tarefa, lista_lixeira):
+    tarefa = lista_tarefas(lista_tarefa)
+    tarefa.apaga_tarefa(lista_lixeira)
+
+def refazer(lista_tarefa, lista_lixeira):
+    tarefa = lista_tarefas(lista_tarefa)
+    tarefa.refaz_tarefa(lista_lixeira)
 
 def main():
     lista_tarefa = []
     lista_lixeira = []
 
-    opcoes = input('Comandos: Listar, Desfazer e Refazer\nDigite sua tarefa ou um comando:').lower()
+    while True:
+        opcoes = input('Comandos: Listar, Desfazer e Refazer\nDigite sua tarefa ou um comando:').lower()
 
+        if opcoes == 'listar':
+            if not lista_tarefa:
+                print('Lista vazia.')
+            else:
+                listar(lista_tarefa)
+                print()
+        elif opcoes == 'desfazer':
+            if not lista_tarefa:
+                print('Nada a desfazer.\n')
+            else:
+                desfazer(lista_lixeira, lista_lixeira)
+                print()
+        elif opcoes == 'refazer':
+            if not lista_tarefa and not lista_lixeira:
+                print('Nada a refazer.\n')
+            else:
+                refazer(lista_tarefa, lista_lixeira)
+                print()
+        elif opcoes == 'sair':
+            break
+        elif opcoes == 'limpar':
+            os.system('cls')
+        else:
+            inserir_tarefa(lista_tarefa, opcoes)
     
-    if opcoes == 'listar':
-        listar(lista_tarefas)
-    elif opcoes == 'desfazer':
-        pass
-    elif opcoes == 'refazer':
-        pass
-    else:
-        pass
-    
-
 main()
