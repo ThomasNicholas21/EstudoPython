26df428da23fdd2f7024c7828cc1c59ca585c313
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-09-23 16:13:19 -0300

    Criando função de edição de carro

--- exercicios/poo/exercicio_carro.py
+++ exercicios/poo/exercicio_carro.py
@@ -47,14 +47,88 @@ def cadastro_carro(lista_de_carro):
     motor = input('Motor do carro: ')
     fabricante = input('Fabricante do carro: ')
 
-    carro1, motor1, fabricante1 = Carro(carro), Motor(motor), Fabricante(fabricante)
-    carro1.motor, carro1.fabricante = motor1, fabricante1
+    carro, motor, fabricante = Carro(carro), Motor(motor), Fabricante(fabricante)
+    carro.motor, carro.fabricante = motor, fabricante
 
-    lista_de_carro.append(carro1)
+    lista_de_carro.append(carro)
 
 def listar_carros(lista_de_carro):
     for carro in lista_de_carro:
         print(carro)
+
+def edit_carro_cadastrado(lista_de_carro):
+    for carro in lista_de_carro:
+        print(carro.nome)
+        print()
+    
+        selecao = input('Qual carro deseja editar ou deletar?\n'
+                        '-->')
+        
+        if selecao == carro.nome:
+            alteracao = input('Deseja alterar o nome, motor ou fabricante?\n'
+                                'N - Nome\n'
+                                'M - Motor\n'
+                                'F - Fabricante\n'
+                                'S - Sair\n'
+                                '-->').lower()
+                                
+            if alteracao == 'n':
+                op = input('Editar ou excluir?\n'
+                           'E - Edidar\n'
+                           'D - Deletar\n'
+                           '--> ')
+                
+                if op == 'e':
+                    novo_nome = input('Novo nome: ')
+                    carro.nome = novo_nome
+
+                elif op == 'd':
+                    del carro.nome
+
+                else:
+                    print('Comando invalido')
+            
+            elif alteracao == 'm':
+                op = input('Editar ou excluir?\n'
+                           'E - Edidar\n'
+                           'D - Deletar\n'
+                           '--> ')
+                
+                if op == 'e':
+                    novo_motor = input('Novo motor: ')
+                    carro.motor = novo_motor
+
+                elif op == 'd':
+                    del carro.motor
+
+                else:
+                    print('Comando invalido')
+
+            elif alteracao == 'f':
+                op = input('Editar ou excluir?\n'
+                           'E - Edidar\n'
+                           'D - Deletar\n'
+                           '--> ')
+                
+                if op == 'e':
+                    novo_fabricante = input('Novo fabricante: ')
+                    carro.fabricante = novo_fabricante
+
+                elif op == 'd':
+                    del carro.fabricante
+
+                else:
+                    print('Comando invalido')
+            
+            elif alteracao == 's':
+                break
+
+            else:
+                print('Comando inválido.')
+        
+        else:
+            print('Comando inválido.')
+
 
 def main():
     lista_carro_cadastrado = []
@@ -63,6 +137,7 @@ def main():
         op = input('Deseja cadastrar, listar, editar ou sair?\n'
                    'C - Cadastrar\n'
                    'L - Listar\n'
+                   'E - Listar\n'
                    'S - Sair\n'
                    '--> ').lower()
 
@@ -73,7 +148,7 @@ def main():
             listar_carros(lista_carro_cadastrado)
             print()
         elif op == 'e':
-            pass
+            edit_carro_cadastrado(lista_carro_cadastrado)
         elif op == 's':
             break
         else:
