05e01cd5933f583ce4a4ebbffcf35cfb2b1665df
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-09-30 21:14:03 -0300

    Henraça, mixin part 2

--- exercicios/poo/log/log.py
+++ exercicios/poo/log/log.py
@@ -1,14 +1,27 @@
 # criando log utilizando abstra;áo
 
 class Log:
-    def log(self, msg): # Assinatura
+    def _log(self, msg): # Assinatura
         raise NotImplementedError('Implemente o método log')
     
+    def log_error(self, msg):
+        return self._log(f'Error: {msg}')
+    
+    def log_sucesso(self, msg):
+        return self._log(f'Sucess: {msg}')
+    
 class LogFIleMixin(Log): # Mixin é uma convenção que indica que o mesmo não será da familia do objeto, pois é uma classe que adiciona funcionalidade em outras classes
-    def log(self, msg): # Assinatura
+    def _log(self, msg): # Assinatura
         print(msg)
 
+class LogPrintMixin(Log): 
+    def _log(self, msg): # Assinatura
+        print(f'{msg} ({self.__class__.__name__})')
 
 if __name__ == '__main__': # Irá executar apenas no arquivo __main__
-    l = LogFIleMixin()
-    l.log('Teste')
+    l1 = LogFIleMixin()
+    l2 = LogPrintMixin()
+    l1.log_error('Mensagem de erro')
+    l1.log_sucesso('Mensagem de sucesso')
+    l2.log_error('Mensagem de erro')
+    l2.log_sucesso('Mensagem de sucesso')
