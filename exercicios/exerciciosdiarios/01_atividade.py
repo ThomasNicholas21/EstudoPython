0a26840098883dd67288e75d1254795d587181b6
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-10-02 19:15:33 -0300

    Exercicio diário para praticar - 01

---
+++ exercicios/exerciciosdiarios/01_atividade.py
@@ -0,0 +1,18 @@
+# Dia 1: Estruturas Condicionais
+# Crie um programa que leia a idade de uma pessoa e informe se ela pode votar ou não.
+
+# Necessário para votar no Brasil
+# Idade minima: 16 e 17, maiores de 70 anos e analfabetos - Voto Facultativo
+# Idade minima: 18 aos 70 anos - Voto Obrigatório
+# Nacionalidade: Ser Brasileiro ou naturalizado
+# Titulo de Eleitor: Numero de identificação
+# Documento de Identificação: Utilizar CPF ou RG 
+# Capacidade Civil: direitos políticos suspensos (condenação eleitoral)
+
+# Esse programa Verifica se a pessoa está apta a votar, não realiza consultas externas
+
+class Pessoa:
+    ...
+
+class NecessarioParaVotar:
+    ...
