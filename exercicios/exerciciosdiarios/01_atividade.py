8cf34a84932eabe813213889685983ede90e81a5
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-10-03 22:24:35 -0300

    ncluindo método de verificar titulo e criando classe destrutora

--- exercicios/exerciciosdiarios/01_atividade.py
+++ exercicios/exerciciosdiarios/01_atividade.py
@@ -28,7 +28,16 @@ class NecessarioParaVotar:
         if validacao == cpf:
             print('CPF confirmado, possui nacionalidade.')
             return True
+        
         return False
+    
+    def verifica_titulo(self):
+        inscricao_titulo = input('Insira sua inscriação de titulo: ')
+        if 0 < len(inscricao_titulo) <= 6:
+            return True
+        
+        return False
+            
     
 class Pessoa:
     def __init__(self, nome, cpf, ano_nascimento) -> None:
@@ -38,13 +47,16 @@ class Pessoa:
         self.necessario_para_votar = NecessarioParaVotar()
 
     def pode_votar(self):
-        if self.necessario_para_votar.idade_minima(self._ano_nascimento) and self.necessario_para_votar.verifica_nacionalidade(self._cpf):
+        if self.necessario_para_votar.idade_minima(self._ano_nascimento) and self.necessario_para_votar.verifica_nacionalidade(self._cpf) and self.necessario_para_votar.verifica_titulo() :
             print(f'{self._nome} pode votar')
 
+    def __del__(self):
+        print('Dados sendo destruídos após validação!')
 
 if __name__ == '__main__':
     user_test = Pessoa('Fulano', '123456789', 2000)
     user_test.pode_votar()
-    user_test.necessario_para_votar.idade_minima(2000)
-    user_test.necessario_para_votar.verifica_nacionalidade('123456789')
-    
+    #user_test.necessario_para_votar.idade_minima(2000)
+    #user_test.necessario_para_votar.verifica_nacionalidade('123456789')
+    #user_test.necessario_para_votar.verifica_titulo()
+    del user_test
