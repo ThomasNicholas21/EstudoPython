9413b72254320fcc3666ef2118815d9c6e2f770b
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-10-07 11:14:58 -0300

    Estudando como criar uma tabela com python em sql

--- estudos/04_BD/introducao_bd.py
+++ estudos/04_BD/introducao_bd.py
@@ -4,11 +4,23 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).parent
 DB_NOME = 'db_estudo.sqlite3'
 DB_FILE = ROOT_DIR / DB_NOME # Realiza a concatenação entre o caminho do arquivo e o nome da database
+TABLE_NAME = 'customers'
 
 connection = sqlite3.connect(DB_FILE)  # Iniciar a conecção
 cursor = connection.cursor() # Cursor executa consulta, seleciona, elimina e etc
 
 # SQL
+
+#Será responsável por executar querys
+cursor.execute(
+    F'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
+    '('
+    'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+    'name TEXT,'
+    'weight REAL'
+    ')'
+)
+connection.commit() # Faz o commit das informaçoes
 
 # Sempre é necessário fechar aquilo que se for aberto para evitar erros
 cursor.close()
