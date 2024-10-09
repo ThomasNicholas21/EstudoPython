e34c921aed5fcf571467d47f38f6939e8ca676b4
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-10-08 22:52:51 -0300

    Atividade diária 3 segunda resolução

--- exercicios/exerciciosdiarios/03_atividade.py
+++ exercicios/exerciciosdiarios/03_atividade.py
@@ -1,16 +1,23 @@
 # Dia 3: Estruturas de Repetição (For)
 # Crie um programa que exiba a tabuada de um número informado pelo usuário.
+class Tabuada:
+    def __init__(self, numero: int) -> None:
+        self.numero = numero
+    
+    def realiza_tabuada(self):
+        list_tabuada = [f'{self.numero} x {item} = {self.numero*item}' for item in range(1, 11)]
+        return '\n'.join(list_tabuada)
 
-def tabuada(numero: int):
-    texto_tabuda = ''
-    for item in range(1, 11, 1):
-        texto_tabuda += f'{numero} x {item} = {numero*item}\n'
-    return texto_tabuda
+# def tabuada(numero: int):
+#     texto_tabuda = ''
+#     for item in range(1, 11, 1):
+#         texto_tabuda += f'{numero} x {item} = {numero*item}\n'
+#     return texto_tabuda
 
 def main():
     try:
         numero = int(input('Insira o número para saber a tabuada: '))
-        print(tabuada(numero))
+        print(Tabuada(numero).realiza_tabuada())
     except ValueError as v:
         print('Número inváido, erro: ', v)
     finally:
