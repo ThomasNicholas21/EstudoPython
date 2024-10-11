4f646c6608df7f3986e1fd7c67e51dcefb007580
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-10-11 18:59:11 -0300

    Atividade 06

---
+++ exercicios/exerciciosdiarios/06_atividade.py
@@ -0,0 +1,3 @@
+# Dia 6: Dicionários
+# Faça um programa que gerencie o estoque de uma loja, onde o usuário pode adicionar ou 
+# remover itens do estoque, além de consultar a quantidade disponível de um item específico.
