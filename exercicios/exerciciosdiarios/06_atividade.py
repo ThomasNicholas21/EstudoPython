8393c6ae3823097399bfb120a85766b75430001b
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-10-11 22:29:36 -0300

    Estruturação do Código

--- exercicios/exerciciosdiarios/06_atividade.py
+++ exercicios/exerciciosdiarios/06_atividade.py
@@ -1,3 +1,21 @@
 # Dia 6: Dicionários
 # Faça um programa que gerencie o estoque de uma loja, onde o usuário pode adicionar ou 
 # remover itens do estoque, além de consultar a quantidade disponível de um item específico.
+
+class Estoque:
+    ...
+
+def inserir_produto():
+    ...
+
+def remover_produto():
+    ...
+
+def consultar_produto():
+    ...
+
+def main():
+    ...
+
+if __name__ == "__main__":
+    main() 
