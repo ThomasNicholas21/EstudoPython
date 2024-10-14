fd8a002ea7f7d021983330903f1ef2e942cd6e22
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-10-13 22:48:24 -0300

    Adicionando método de rmeover produto

--- exercicios/exerciciosdiarios/06_atividade.py
+++ exercicios/exerciciosdiarios/06_atividade.py
@@ -5,6 +5,9 @@
 class Produto:
     def __init__(self, produtos: dict=None) -> None:
         self.produtos = produtos
+    
+    def remove_produto(self, valor):
+        self.produtos.pop(valor)
 
     def __repr__(self) -> str:
         return f'{'\n'.join([f'Produto: {chave} - Quantidade: {valor}' for chave, valor in self.produtos.items()])}'
@@ -21,10 +24,11 @@ class Estoque:
         return self.lista_dict
 
     def deletar_estoque(self):
-        if self.produtos is None:
+        if not self.produtos:
             del self.lista_dict
         else:
             print('Estoque possui produtos:', self.produtos, 'Estoque não deletado')
+            print(bool(self.produtos))
     
     def __repr__(self) -> str:
         return f'{self.nome_estoque.capitalize()}: \n{self.produtos}'
@@ -43,10 +47,16 @@ def main():
     nome_estoque = 'estoque_teste1'
     produtos = Produto({'havaina_teste1': 22, 'havaina_teste2': 23})
     estoque1 = Estoque(nome_estoque, produtos, estoque_dict)
-    estoque1.criar_estoque()
+    estoque_dict = estoque1.criar_estoque()
     print(estoque1)
+    
+    produtos.remove_produto('havaina_teste1')
+    produtos.remove_produto('havaina_teste2')
+
+    estoque1.deletar_estoque()
     #estoque1.deletar_estoque()
     #print(estoque1)
+    print(estoque_dict)
 
 if __name__ == "__main__":
     main() 
