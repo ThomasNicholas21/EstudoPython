c131c3cfbfc9e0e8f7e7e5629c989878e86487ff
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-10-15 20:00:49 -0300

    Corrigindo codigo e criando função consultar produto

--- exercicios/exerciciosdiarios/06_atividade.py
+++ exercicios/exerciciosdiarios/06_atividade.py
@@ -1,26 +1,25 @@
 # Dia 6: Dicionários
 # Faça um programa que gerencie o estoque de uma loja, onde o usuário pode criar um estoque, adicionar ou 
-# remover itens do estoque, editar estoque, além de consultar a quantidade disponível de um item específico.
+# remover itens do estoque, além de consultar a quantidade disponível de um item específico.
 
 class Produto:
-    def __init__(self, produtos: dict=None) -> None:
-        self.produtos = produtos
+    def __init__(self, produtos: dict=None, estoque_dict: dict=None) -> None:
+        self.produtos = produtos if produtos else {}
+        self.estoque_dict = estoque_dict
 
-    def __repr__(self) -> str:
-        return f'{'\n'.join([f'Produto: {chave} - Quantidade: {valor}' for chave, valor in self.produtos.items()])}'
-    
+    def inserir_produto(self, nome_chave):
+        self.estoque_dict[nome_chave].update({**self.produtos})
+  
 class Estoque:
     def __init__(self, nome_estoque, produtos: dict=None, lista_dict: dict=None) -> None:
         self.nome_estoque = nome_estoque
-        self.produtos = produtos
+        self.produtos = produtos 
         if lista_dict is not None:
             self.lista_dict = {}
 
     def criar_estoque(self, estoque_dict):
-        estoque_dict.update({self.nome_estoque: {self.produtos}})
-
-    def __repr__(self) -> str:
-        return f'{self.nome_estoque.capitalize()}: \n{self.produtos}'
+        if self.produtos is None:
+            estoque_dict.update({self.nome_estoque: {}})
 
 def procura_chave(chave, estoque_dict: dict) -> bool:
     if chave in estoque_dict:
@@ -39,17 +38,18 @@ def inserir_produto(estoque_dict: dict):
         if procura_chave(nome_chave, estoque_dict):
             nome_produto = input('Nome do produto: ')
             quantidade_produto = int(input('Quantidade do produto: '))
-            produto = Produto({nome_produto: quantidade_produto})
-            estoque_dict[nome_chave].update({produto})
-            
+            produto = Produto({nome_produto: quantidade_produto}, estoque_dict)
+            produto.inserir_produto(nome_chave)
             print()
             
         else:
             print('Estoque não encontrado')
+            print()
             return
 
     except ValueError:
-        raise print(f'A quantidade do produto: {quantidade_produto} de ser um número - ', ValueError)
+        raise print(f'A quantidade do produto: {quantidade_produto} de ser um número - ', ValueError, end='\n')
+        
 
 def remover_produto(estoque_dict):
     try:
@@ -66,14 +66,20 @@ def remover_produto(estoque_dict):
         print('Erro: ', e)
 
 def consultar_produto(estoque_dict):
-    ...
+    nome_estoque = input('Digite o nome do estoque: ')
+    if nome_estoque in estoque_dict: 
+        nome_produto = input('Digite o nome do produto: ')
+        if nome_produto in estoque_dict[nome_estoque]:
+            print(estoque_dict[nome_estoque][nome_produto].items())
+        else:
+            print('Produto não encontrado!')
 
 def main():
     estoque_dict = {}
     
     while True:
         comandos = input('Comandos: \ncriar estoque [ce]\nadicionar produto [ap]\n' 
-        'remover produto [rp]\ndeletar estoque [de]\neditar estoque [ee]\nconsultar estoque [c]\nsair [s]\n-->')
+        'remover produto [rp]\ndeletar estoque [de]\nconsultar estoque [c]\nsair [s]\n-->')
 
         if comandos == 'ce':
             criar_estoque(estoque_dict)
@@ -85,6 +91,7 @@ def main():
             ...
         elif comandos == 'c':
             print(estoque_dict)
+            consultar_produto(estoque_dict)
             print()
         elif comandos == 's':
             break
@@ -92,4 +99,4 @@ def main():
             print('Comando invalido!')
 
 if __name__ == "__main__":
-    main() 
+    main()  
