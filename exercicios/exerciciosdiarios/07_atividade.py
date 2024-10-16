317e9c7ca194cf475b077f866d8007d7ef5ac128
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-10-16 19:49:15 -0300

    Refatoração do código: Alterando classe e atribuindo função

--- exercicios/exerciciosdiarios/07_atividade.py
+++ exercicios/exerciciosdiarios/07_atividade.py
@@ -3,20 +3,19 @@
 import random
 
 class GeradorSet:
-   def gerar_numero_aleatorios(self, start, stop, quantidade, lista_set):
-        numero = [random.randint(start, stop) for number in range(quantidade)]
-        lista_set = {*numero}
-        return lista_set
+   def gerar_numero_aleatorios(self, start, stop, quantidade):
+        return {random.randint(start, stop) for number in range(quantidade)}
         
-def inserir_parametro(lista_set: set):
+def inserir_parametro():
     try:
         print(f'Iniciando definição de intervalo de números aleatórios')
         comeco = int(input('Entre: '))
         final = int(input('E: '))
         quantidade = int(input('Quantos números devem ser gerados: '))
-        if comeco <= final and quantidade > 0:
+
+        if comeco <= final and quantidade > 0 :
             gerador = GeradorSet()
-            lista_set = gerador.gerar_numero_aleatorios(comeco, final, quantidade, lista_set)
+            lista_set = gerador.gerar_numero_aleatorios(comeco, final, quantidade)
             print('Lista criada com sucesso!')
             return lista_set
         
@@ -25,16 +24,16 @@ def inserir_parametro(lista_set: set):
             return
         
     except ValueError as v:
-        raise print(f'Deve ser inserido números inteiros, erro: {v}')
+        print(f'Deve ser inserido números inteiros, erro: {v}')
+        return 
+# raise realiza lançamentos diretos
 
 def main():
-    lista_set1 = {}
-    lista_set2 = {}
+    lista_set1 = inserir_parametro()
+    lista_set2 = inserir_parametro()
 
-    lista_set1 = inserir_parametro(lista_set1)
-    lista_set2 = inserir_parametro(lista_set2)
     if lista_set1 and lista_set2:
-        print(f'Lista 1 {lista_set1} e lista 2 {lista_set2}, intersecção: {lista_set1.intersection(lista_set2)}')
+        print(f'Lista 1 {lista_set1} e lista 2 {lista_set2}, intersecção: {lista_set1.intersection(lista_set2) if lista_set1.intersection(lista_set2) else 'Nenhuma intersecção'}')
     else:
         print('Ocorreu um erro.')
 
