760ddf35e914a5bc22b440f2f1a7205a16218e40
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-10-16 19:31:57 -0300

    Exercicio 7 - primeira versão

--- exercicios/exerciciosdiarios/07_atividade.py
+++ exercicios/exerciciosdiarios/07_atividade.py
@@ -3,29 +3,40 @@
 import random
 
 class GeradorSet:
-    def gerar_numero_aleatorios(self, start, stop, quantidade, lista: set):
+   def gerar_numero_aleatorios(self, start, stop, quantidade, lista_set):
         numero = [random.randint(start, stop) for number in range(quantidade)]
-        lista = {*numero}
-        return lista
-
-def inserir_parametro(lista_set: GeradorSet):
+        lista_set = {*numero}
+        return lista_set
+        
+def inserir_parametro(lista_set: set):
     try:
-        print('Iniciando definição de intervalo de números aleatórios')
+        print(f'Iniciando definição de intervalo de números aleatórios')
         comeco = int(input('Entre: '))
         final = int(input('E: '))
         quantidade = int(input('Quantos números devem ser gerados: '))
-        lista_set.gerar_numero_aleatorios(comeco, final, quantidade, lista_set)
-
+        if comeco <= final and quantidade > 0:
+            gerador = GeradorSet()
+            lista_set = gerador.gerar_numero_aleatorios(comeco, final, quantidade, lista_set)
+            print('Lista criada com sucesso!')
+            return lista_set
+        
+        else:
+            print('Erro, começo deve ser menor que o final ou a quantidade deve ser maior que 0')
+            return
+        
     except ValueError as v:
         raise print(f'Deve ser inserido números inteiros, erro: {v}')
 
 def main():
-    lista_set1 = GeradorSet()
-    lista_set2 = GeradorSet()
+    lista_set1 = {}
+    lista_set2 = {}
 
-    inserir_parametro(lista_set1)
-    print(lista_set1)
-
+    lista_set1 = inserir_parametro(lista_set1)
+    lista_set2 = inserir_parametro(lista_set2)
+    if lista_set1 and lista_set2:
+        print(f'Lista 1 {lista_set1} e lista 2 {lista_set2}, intersecção: {lista_set1.intersection(lista_set2)}')
+    else:
+        print('Ocorreu um erro.')
 
 if __name__ == '__main__':
     main()
