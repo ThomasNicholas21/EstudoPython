c34e996d611a87575fef30fd1153f163e7a0d252
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-10-17 20:26:31 -0300

    Aprendendo a utilizar DocStrings para funções

---
+++ estudos/modulos/01_DIR_HELP/doc2.py
@@ -0,0 +1,27 @@
+# estudo sobre DocStrings
+""" Começo do código -> indica o título
+
+Esse código irá possuir funções de soma
+"""
+
+def soma(x: int | float, y: int | float) -> int | float: # Type anotation
+    """
+    Está função realiza soma dos parâmetros
+
+    :param x:Número 1
+    :type x: int or float
+    :param y:Número 2
+    :type y: int or float
+
+    return: A soma entre x e y
+    :rtype: int or float
+
+    """# Tipo de anotação parecida com TypeAnotation
+    return x + y
+
+# Aparecem como dados 
+variavel1 = 10
+variavel2 = 15
+
+resultado = soma(variavel1, variavel2)
+print(resultado)
