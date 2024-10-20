2025ca4ad71e4b6cc8fa9f14c301dcd645cfd768
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-10-20 18:40:14 -0300

    atividade 12 - primeira versão

--- exercicios/exerciciosdiarios/12_atividade.py
+++ exercicios/exerciciosdiarios/12_atividade.py
@@ -8,17 +8,29 @@ class MyReader:
         self._abrir_arquivo = None
 
     def __enter__(self):
+        print('Iniciando processo de leitura.')
         self._abrir_arquivo = open(self.nome_arquivo, self.modo, encoding='utf8')
         return self._abrir_arquivo
     
-    def __exit__(self):
+    def __exit__(self, class_exception_, exception_, traceback_):
         print('Finalizando processo de leitura.')
+        self._abrir_arquivo.close()
 
 def contador_de_palavras():
-    ...
+    with MyReader('D:/programação/EstudoPython/exercicios/exerciciosdiarios/12_atividade.txt') as arquivo:
+        contador = 1
+        for lines in arquivo.readlines():
+            palavras = lines.split(' ')
+            for palavra in palavras:
+                if not palavra == '\n':
+                    print(palavra)
+                    contador += 1
+        return contador
+
 
 def main():
-    ...
+    quantidade_palavras = contador_de_palavras()
+    print(f'A quantidade de palvras no arquivo de texto: {quantidade_palavras}')
 
 
 if __name__ == "__main__":
