acab7c4fbf220642a42a469a56e66904c1051447
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-10-19 22:37:27 -0300

    Atividade 11 pimeira versão

--- exercicios/exerciciosdiarios/11_atividade.py
+++ exercicios/exerciciosdiarios/11_atividade.py
@@ -2,10 +2,16 @@
 # Crie um programa que inverta uma string informada pelo usuário, sem usar funções 
 
 class InverteString:
-    ...
+    def __init__(self, string: str) -> None:
+        self.string = string
+    
+    def __repr__(self) -> str:
+        return f'{self.string[::-1]}'
 
 def main():
-    ...
-    
+    string = input('Digite qualquer coisa: ')
+    string_invertida = InverteString(string)
+    print(f'Iniciando processo de inversão...\n{string} | {string_invertida}')
+
 if __name__ == "__main__":
     main()
