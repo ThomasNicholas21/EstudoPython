8696ebb875d7d5146f4d4bcd88e16061446b295f
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-10-19 22:28:56 -0300

    Definindo estrutura

---
+++ exercicios/exerciciosdiarios/11_atividade.py
@@ -0,0 +1,11 @@
+# Dia 11: Manipulação de Strings
+# Crie um programa que inverta uma string informada pelo usuário, sem usar funções 
+
+class InverteString:
+    ...
+
+def main():
+    ...
+    
+if __name__ == "__main__":
+    main()
