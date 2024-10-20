ee6c6c7b072ab4e4f25e6a84cf8559cd672d0641
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-10-20 17:51:51 -0300

    Definindo classe de leitura com base em duck typing

--- exercicios/exerciciosdiarios/12_atividade.py
+++ exercicios/exerciciosdiarios/12_atividade.py
@@ -5,13 +5,17 @@ class MyReader:
     def __init__(self, nome_arquivo):
         self.nome_arquivo = nome_arquivo
         self.modo = 'r'
+        self._abrir_arquivo = None
 
     def __enter__(self):
-        ...
+        self._abrir_arquivo = open(self.nome_arquivo, self.modo, encoding='utf8')
+        return self._abrir_arquivo
     
     def __exit__(self):
-        ...
+        print('Finalizando processo de leitura.')
 
+def contador_de_palavras():
+    ...
 
 def main():
     ...
