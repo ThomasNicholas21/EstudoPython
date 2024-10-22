a02357db333d700f206a7f3325e650ae86bd0cd5
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-10-21 21:19:13 -0300

    Definindo métodos de Tarefa e OpenText

--- exercicios/exerciciosdiarios/13_atividade.py
+++ exercicios/exerciciosdiarios/13_atividade.py
@@ -2,18 +2,20 @@
 # Faça um programa que pergunte ao usuário por uma lista de tarefas e as salve em um arquivo de texto.
 
 class Tarefas:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, tarefa) -> None:
+        self.tarefa = tarefa
 
-    def criar_tarefa(self):
-        pass
+    def criar_tarefa(self, lista: list):
+        lista.append(self.tarefa)
 
-    def deletar_tarefa(self):
-        pass
+    def deletar_tarefa(self, lista: list):
+        lista.remove(self.tarefa)
 
 class GeraTarefaTXT:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, caminho_arquivo) -> None:
+        self.caminho_arquivo = caminho_arquivo
+        self.modo = 'w'
+        self.abrir_arquivo = None
 
     def __enter__(self):
         pass
