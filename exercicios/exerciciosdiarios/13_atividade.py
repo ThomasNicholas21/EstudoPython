235567f76e62f4cf610712c837d1d849ff11f7d7
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-10-21 21:09:16 -0300

    Estruturação atividade

---
+++ exercicios/exerciciosdiarios/13_atividade.py
@@ -0,0 +1,28 @@
+# Dia 13: Arquivos (Escrita)
+# Faça um programa que pergunte ao usuário por uma lista de tarefas e as salve em um arquivo de texto.
+
+class Tarefas:
+    def __init__(self) -> None:
+        pass
+
+    def criar_tarefa(self):
+        pass
+
+    def deletar_tarefa(self):
+        pass
+
+class GeraTarefaTXT:
+    def __init__(self) -> None:
+        pass
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self):
+        pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
