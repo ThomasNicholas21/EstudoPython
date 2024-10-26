700a66e3e2edfb031e8e2be64a08008a8853df46
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-10-25 23:35:41 -0300

    Definindo estrutura atividade 14

---
+++ exercicios/exerciciosdiarios/14_atividade.py
@@ -0,0 +1,29 @@
+# Dia 14: Tratamento de Exceções
+# Desenvolva um calculadora que peça dois números ao usuário, e faça a divisão tratando a exceção de divisão por zero.
+def divisao_por_zero(self):
+    ...
+
+def verificacao_string(self):
+    ...
+
+class Calculadora:
+    def soma(self):
+        ...
+    
+    def subtracao(self):
+        ...
+
+    def multiplicacao(self):
+        ...
+    
+    def divisao(self):
+        ...
+    
+def calculadora():
+    ...
+
+def main():
+    ...
+
+if __name__ == '__main__':
+    main()
