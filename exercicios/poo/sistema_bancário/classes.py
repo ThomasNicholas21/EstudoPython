8f2191d29804038f7982867912082d485b37b837
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-10-30 22:09:27 -0300

    Definindo classe cliente e realizando testes com método getter

--- exercicios/poo/sistema_bancário/classes.py
+++ exercicios/poo/sistema_bancário/classes.py
@@ -1,7 +1,27 @@
 from abc import ABC, abstractmethod
 
 class Pessoa:
-    ...
+    def __init__(self, nome, idade, cpf, cep) -> None:
+        self._nome = nome
+        self._idade = idade
+        self._cpf = cpf
+        self._cep = cep
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @property
+    def idade(self):
+        return self._idade
+    
+    @property
+    def cpf(self):
+        return self._cpf
+    
+    @property
+    def cep(self):
+        return self._cep
 
 class Cliente(Pessoa): # Agrega classe Conta
     ...
@@ -38,3 +58,10 @@ class ContaPoupanca(Conta):
 
 class Banco: # agrega clientes e contas
     ...
+
+if __name__ == '__main__':
+    cliente1 = Pessoa('Thomas', 22, '123456789', '123456')
+    print(cliente1.nome)
+    print(cliente1.idade)
+    print(cliente1.cpf)
+    print(cliente1.cep)
