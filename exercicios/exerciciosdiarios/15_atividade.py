db6926cc14d1a3ec849d0dbdcd1b3f72f6f365be
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-11-06 22:40:57 -0300

    Definindo estrutura da atividade 15 - agenda telefônica

---
+++ exercicios/exerciciosdiarios/15_atividade.py
@@ -0,0 +1,21 @@
+# Dia 15: Estruturas Aninhadas
+# Faça um programa que simule uma agenda telefônica, 
+# onde o usuário pode cadastrar contatos (nome e número) e buscar um contato pelo nome.
+
+class Agenda:
+    ...
+
+class Contato:
+    ...
+
+def cadastro_contato():
+    ...
+
+def buscar_contato():
+    ...
+
+def main():
+    ...
+
+if __name__ == '__main__':
+    main()
