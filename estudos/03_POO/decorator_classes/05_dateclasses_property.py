a0a78390467f41f01cc1807121db57b53ab351a4
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-11-12 22:01:31 -0300

    Testando property com dateclass

--- estudos/03_POO/decorator_classes/05_dateclasses_property.py
+++ estudos/03_POO/decorator_classes/05_dateclasses_property.py
@@ -8,11 +8,16 @@ class Pessoa:
 
     @property
     def nome_completo(self) -> str:
-        return f'{self.nome}{self.sobrenome}'
+        return f'{self.nome} {self.sobrenome}'
     
     @nome_completo.setter
     def nome_completo(self, valor: str) -> str:
         nome, *sobrenome = valor.split()
         self.nome = nome
-        self.nome = sobrenome
+        self.sobrenome = ' '.join(sobrenome)
 
+if __name__ == "__main__":
+    pessoa1 = Pessoa('Thomas', 'Pedrosa', 20)
+    print(pessoa1.nome_completo)
+    pessoa1.nome_completo = 'Thomas Pedrosa Matias'
+    print(pessoa1.nome_completo)
