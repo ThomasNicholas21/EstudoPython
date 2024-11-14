7594c214a481e199ed39772f409244a57bd697f3
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-11-13 22:21:19 -0300

    Definind classes e suas comunicações: Agregação

--- exercicios/exerciciosdiarios/15_atividade.py
+++ exercicios/exerciciosdiarios/15_atividade.py
@@ -1,14 +1,16 @@
 # Dia 15: Estruturas Aninhadas
 # Faça um programa que simule uma agenda telefônica, 
 # onde o usuário pode cadastrar contatos (nome e número) e buscar um contato pelo nome.
+from dataclasses import dataclass
 
-class Agenda:
-    ...
-
+@dataclass
 class Contato:
-    def __init__(self, nome: str, numero: str) -> None:
-        self.nome = nome
-        self.numero = numero
+    nome: str
+    numero: str 
+
+@dataclass
+class Agenda_telefonica:
+    lista_contatos: list[Contato]
 
 def cadastro_contato():
     ...
@@ -21,3 +23,7 @@ def main():
 
 if __name__ == '__main__':
     main()
+    contato_teste1 = Contato('Fulano', '62982520334')
+    lista_contatos = [contato_teste1]
+    agenda = Agenda_telefonica(lista_contatos)
+    print(contato_teste1, agenda)
