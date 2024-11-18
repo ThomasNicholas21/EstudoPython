3fd6eb23c807a26ffcb9cd968c263d6a6b6aed3c
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-11-18 19:48:31 -0300

    Estudando diretivas de datetime

--- estudos/modulos/05_DATETIME/01_datetime.py
+++ estudos/modulos/05_DATETIME/01_datetime.py
@@ -10,6 +10,7 @@
 # https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 # Instalando o pytz
 # pip install pytz types-pytz
+
 from datetime import datetime
 
 if __name__ == "__main__":
@@ -18,4 +19,10 @@ if __name__ == "__main__":
     data1 = datetime(2000, 12, 31)
     data2 = datetime(2000, 12, 31, 23, 59, 59, 999999)
     print(data1, data2, sep='\n')
+    #utilizando diretivas do datetime
+    data3 = '2000-12-20 01:12:54'
+    data_formato1 = '%Y-%m-%d %H:%M:%S'
+    data_formato2 = '%y-%B-%a %I:%M:%S %p'
+    print(datetime.strptime(data3, data_formato1))
+    print(datetime.now().strftime(data_formato2))
 
