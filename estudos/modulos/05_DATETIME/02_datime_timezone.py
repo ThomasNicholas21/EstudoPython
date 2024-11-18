70f9b53afbc5ce6891cbe25b0ac2b3d113fcfa0c
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-11-18 20:27:18 -0300

    Estudando bibliotecas pytz e time delta, para manipular timezones

--- estudos/modulos/05_DATETIME/02_datime_timezone.py
+++ estudos/modulos/05_DATETIME/02_datime_timezone.py
@@ -1,8 +1,13 @@
-from datetime import datetime
-from pytz import timezone
+from datetime import datetime, timedelta, timezone
+from pytz import timezone as tz # simplifica a chamada das timezones
 
 if __name__ == "__main__":
-    data1 = datetime.now(timezone('Asia/Tokyo'))
-    data2 = datetime.now(timezone('Australia/Sydney'))
-    data3 = datetime.now(timezone('America/Denver'))
-    print(data1, data2, data3, sep='\n')
+    data1 = datetime.now(tz('Asia/Tokyo')) 
+    data2 = datetime.now(tz('Australia/Sydney'))
+    data3 = datetime.now(tz('America/Denver'))
+    print(f'Tokyo: {data1}\nAustralia: {data2}\nDenver{data3}')
+
+    fuso_horario = timezone(timedelta(hours=12))
+    data4 = datetime.now(fuso_horario)
+    print(f'Antarctica: {data4}')
+    
