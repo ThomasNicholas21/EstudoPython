286bd7d3d47c7fa600bb4716a092494da52085cd
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-11-18 20:48:12 -0300

    Definindo estrutura da atividade

---
+++ exercicios/exerciciosdiarios/16_atividade.py
@@ -0,0 +1,16 @@
+# Dia 16: Funções Lambda
+# Crie uma lista de dicionários, onde cada dicionário represente 
+# uma pessoa (nome e idade). Use uma função lambda para ordenar 
+# essa lista pela idade.
+
+def main():
+    lista_pessoa = [
+        {'Nome': 'Thomas', 'Idade': 24},
+        {'Nome': 'Fulano', 'Idade': 32},
+        {'Nome': 'João', 'Idade': 12},
+        {'Nome': 'Nicholas', 'Idade': 5},
+        {'Nome': 'Maria', 'Idade': 88}
+    ]
+
+if __name__ == '__main__':
+    main()
