f3e1b708cf5b9d992faf519068fea5baa1eeeeaa
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-11-19 23:56:04 -0300

    Estudando como calcular tempo de uma função com python

---
+++ exercicios/exerciciosdiarios/17_atividade.py
@@ -0,0 +1,14 @@
+# Dia 17: Decoradores
+# Crie um decorador que calcule o tempo de execução de uma função.
+
+#https://docs.python.org/pt-br/3.14/library/time.html
+#https://docs.python.org/pt-br/3.10/library/timeit.html
+
+def calcula_tempo_execucao():
+    ...
+
+def main():
+    ...
+
+if __name__ == '__main__':
+    main()
