db4eaeb458103136fb9b3bf068069881920505c8
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-11-21 21:36:37 -0300

    Estudo de calculo de data

--- estudos/modulos/05_DATETIME/03_datime_timestamp.py
+++ estudos/modulos/05_DATETIME/03_datime_timestamp.py
@@ -1,9 +1,23 @@
 # https://pt.wikipedia.org/wiki/Era_Unix
 # datetime.fromtimestamp(Unix Timestamp)
 # Timestamp Unix contagem de segundos 01/01/1970
-from datetime import datetime
+# https://dateutil.readthedocs.io/en/stable/relativedelta.html
+# https://docs.python.org/3/library/datetime.html#timedelta-objects
+
+from datetime import datetime, timedelta
 
 if __name__ == '__main__':
     data1 = datetime.now()
     print(data1.timestamp()) #  numero de segundos de 1970 até a data atual
     print(datetime.fromtimestamp(0)) # 1969-12-31 21:00:00
+
+    formato = '%d/%m/%Y %H:%M:%S'
+    data2 = datetime.strptime('21/11/2024 21:06:30', formato)
+    data3 = datetime.strptime('31/12/1969 21:00:00', formato)
+    data_dif = data2 - data3
+    print(f'Diferença {data2} e {data3}', data_dif, 
+          data_dif.days, data_dif.seconds, 
+          data_dif.total_seconds(), sep=' | ')
+    
+    data_soma = timedelta(days=10, seconds=5) # timedelta representa duração
+    print(data3 + data_soma)
