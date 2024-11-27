eca533feaba853a2108d9e6216064ffc63684fb7
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-11-26 22:55:24 -0300

    Implementando quick sort

--- exercicios/exerciciosdiarios/17_atividade.py
+++ exercicios/exerciciosdiarios/17_atividade.py
@@ -7,8 +7,32 @@
 def calcula_tempo_execucao():
     ...
 
-def quick_sort():
-    ...
+def quick_sort(lista, inicio, fim):
+    if inicio > fim:
+        return
+    anterior = inicio
+    posterior = fim
+    pivo = lista[inicio]
+
+    while anterior < posterior:
+        while anterior < posterior and lista[posterior] > pivo:
+            posterior = posterior - 1
+
+        if anterior < posterior:
+            lista[anterior] = lista[posterior]
+            anterior = anterior + 1
+
+        while anterior < posterior and lista[anterior] <= pivo:
+            anterior = anterior + 1
+
+        if anterior < posterior:
+            lista[posterior] = lista[anterior]
+            posterior = posterior - 1
+
+        lista[anterior] = pivo
+
+    quick_sort(lista, inicio, anterior - 1)
+    quick_sort(lista, anterior + 1, fim)
 
 def gerador_lista():
     ...
