62510737ba064d219b80b4cfe28bc68729b75097
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-12-09 20:12:05 -0300

    Atividade 18: estruturando codigo

---
+++ exercicios/exerciciosdiarios/18_atividade.py
@@ -0,0 +1,23 @@
+# Dia 18: Classes e Objetos
+# Implemente uma classe Carro com atributos como marca, modelo e ano, e um método para ligar o carro.
+
+class Carro:
+    ...
+
+class Moto:
+    ...
+
+class Aviao:
+    ...
+
+class Barco:
+    ...
+
+def ligar():
+    ...
+
+def main():
+    ...
+
+if __name__ == "__main__":
+    main()
