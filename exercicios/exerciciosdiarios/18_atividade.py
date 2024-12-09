3363ea8e29592beea70c05a8faa57b35911f9f43
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-12-09 20:14:48 -0300

    definindo atributos de carro

--- exercicios/exerciciosdiarios/18_atividade.py
+++ exercicios/exerciciosdiarios/18_atividade.py
@@ -1,8 +1,12 @@
 # Dia 18: Classes e Objetos
 # Implemente uma classe Carro com atributos como marca, modelo e ano, e um método para ligar o carro.
+from datetime import datetime
 
 class Carro:
-    ...
+    def __init__(self, marca: str, modelo: str, ano: datetime) -> None:
+            self.marca = marca
+            self.moodelo = modelo
+            self.ano = ano
 
 class Moto:
     ...
