4d1a390b7a1a09aae79122e7be8d53cd39789def
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-12-10 21:15:57 -0300

    Alteração de variavel de atributos

--- exercicios/exerciciosdiarios/18_atividade.py
+++ exercicios/exerciciosdiarios/18_atividade.py
@@ -3,28 +3,28 @@
 from datetime import datetime
 
 class Carro:
-    def __init__(self, marca: str, modelo: str, ano: datetime) -> None:
+    def __init__(self, marca: str, modelo: str, ano_cadastro: datetime) -> None:
             self.marca = marca
             self.moodelo = modelo
-            self.ano = ano
+            self.ano_cadastro = ano_cadastro
 
 class Moto:
-    def __init__(self, marca: str, modelo: str, ano: datetime) -> None:
+    def __init__(self, marca: str, modelo: str, ano_cadastro: datetime) -> None:
             self.marca = marca
             self.moodelo = modelo
-            self.ano = ano
+            self.ano_cadastro = ano_cadastro
 
 class Aviao:
-    def __init__(self, marca: str, modelo: str, ano: datetime) -> None:
+    def __init__(self, marca: str, modelo: str, ano_cadastro: datetime) -> None:
             self.marca = marca
             self.moodelo = modelo
-            self.ano = ano
+            self.ano_cadastro = ano_cadastro
 
 class Barco:
-    def __init__(self, marca: str, modelo: str, ano: datetime) -> None:
+    def __init__(self, marca: str, modelo: str, ano_cadastro: datetime) -> None:
             self.marca = marca
             self.moodelo = modelo
-            self.ano = ano
+            self.ano_cadastro = ano_cadastro
 
 def ligar(classe):
     print(f'O {classe.__class__.__name__} da marca {classe.marca} está ligando.')
