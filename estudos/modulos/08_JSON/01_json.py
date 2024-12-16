28a0e58f1a79f7554c5976e257008727b43d2ab2
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-12-16 20:10:46 -0300

    Gerando arquivo e definindo arquivo

--- estudos/modulos/08_JSON/01_json.py
+++ estudos/modulos/08_JSON/01_json.py
@@ -11,6 +11,11 @@
 
 import json
 from typing import TypedDict # biblioteca permite que realize a tipagem de um dicionario
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent
+JSON_NAME = 'filme.json'
+FILE = ROOT_DIR / JSON_NAME
 
 class Movie(TypedDict):
     title: str
@@ -40,3 +45,7 @@ def printd(**kwargs):
 if __name__ == "__main__":
     filme_json: Movie = json.loads(filme)
     printd(**filme_json)
+
+    filme_json_string = json.dumps(filme_json, ensure_ascii=False, indent=2)
+    with open(FILE , 'w') as arquivo:
+        json.dump(filme_json_string, arquivo)
