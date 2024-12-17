9780dfcb881f9e4d84326453b8e1717083fef2d9
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-12-17 20:55:41 -0300

    Definindo caminho do CSV

---
+++ estudos/modulos/09_CSV/02_csv2.py
@@ -0,0 +1,6 @@
+# csv.writer e csv.DictWriter para escrever em CSV
+# csv.reader lê o CSV em formato de lista
+# csv.DictReader lê o CSV em formato de dicionário
+from pathlib import Path
+
+CAMINHO_CSV = Path(__file__).parent / '02_csv_arquivo2.csv'
