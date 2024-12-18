4bdf05bc1ce9972721242170565bb62e0ed9a6f3
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-12-17 21:02:32 -0300

    Definindo base de dados

--- estudos/modulos/09_CSV/02_csv2.py
+++ estudos/modulos/09_CSV/02_csv2.py
@@ -4,3 +4,10 @@
 from pathlib import Path
 
 CAMINHO_CSV = Path(__file__).parent / '02_csv_arquivo2.csv'
+
+lista_dicionarios = [
+    {'Nome': 'Thomas', 'Idade': 24, 'Profissão': 'Engenheiro de Software'},
+    {'Nome': 'Pedro', 'Idade': 32, 'Profissão': 'Barbeiro'},
+    {'Nome': 'Lucas', 'Idade': 25, 'Profissão': 'Bombeiro'},
+    {'Nome': 'Lourdes', 'Idade': 85, 'Profissão': 'Engenheira de computação'}
+]
