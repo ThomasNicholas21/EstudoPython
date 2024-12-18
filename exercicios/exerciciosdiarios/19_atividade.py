7d29b7c1177dc1df84e4a821b6237c2b1163dd2f
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-12-18 18:10:37 -0300

    Definindo classes

---
+++ exercicios/exerciciosdiarios/19_atividade.py
@@ -0,0 +1,33 @@
+# Dia 18: Classes e Objetos
+# Implemente uma classe Carro, Moto, Avião, Barco com atributos como marca, modelo e ano.
+# Dia 19: Construtores e Destrutores
+# Expanda o exercício anterior para incluir um método destrutor que exiba uma mensagem ao destruir o objeto.
+
+class MyRepr:
+    def __repr__(self):
+        atributos = [f'{chave}: {valor}' for chave, valor in self.__dict__.items()]
+        return f"{self.__class__.__name__}: {', '.join(atributos)}"
+
+class Carro(MyRepr):
+    def __init__(self, marca: str, modelo: str, ano_cadastro: datetime) -> None:
+            self.marca = marca
+            self.modelo = modelo
+            self.ano_cadastro = ano_cadastro
+
+class Moto(MyRepr):
+    def __init__(self, marca: str, modelo: str, ano_cadastro: datetime) -> None:
+            self.marca = marca
+            self.modelo = modelo
+            self.ano_cadastro = ano_cadastro
+
+class Aviao(MyRepr):
+    def __init__(self, marca: str, modelo: str, ano_cadastro: datetime) -> None:
+            self.marca = marca
+            self.modelo = modelo
+            self.ano_cadastro = ano_cadastro
+
+class Barco(MyRepr):
+    def __init__(self, marca: str, modelo: str, ano_cadastro: datetime) -> None:
+            self.marca = marca
+            self.modelo = modelo
+            self.ano_cadastro = ano_cadastro
