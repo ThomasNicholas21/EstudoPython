4a296ee5e268a4f60420c5df4b4c6285eb4460bc
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-12-17 21:09:55 -0300

    Escrevendo arquivo CSV

--- estudos/modulos/09_CSV/02_csv2.py
+++ estudos/modulos/09_CSV/02_csv2.py
@@ -1,6 +1,7 @@
 # csv.writer e csv.DictWriter para escrever em CSV
 # csv.reader lê o CSV em formato de lista
 # csv.DictReader lê o CSV em formato de dicionário
+import csv
 from pathlib import Path
 
 CAMINHO_CSV = Path(__file__).parent / '02_csv_arquivo2.csv'
@@ -11,3 +12,10 @@ lista_dicionarios = [
     {'Nome': 'Lucas', 'Idade': 25, 'Profissão': 'Bombeiro'},
     {'Nome': 'Lourdes', 'Idade': 85, 'Profissão': 'Engenheira de computação'}
 ]
+
+with open(CAMINHO_CSV, 'w', encoding='utf-8') as arquivo:
+    escritor = csv.writer(arquivo)
+    nome_coluna = lista_dicionarios[0].keys()
+    print(nome_coluna)
+
+    escritor.writerow(nome_coluna)
