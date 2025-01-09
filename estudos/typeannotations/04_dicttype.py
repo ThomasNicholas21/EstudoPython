eda7733473b580b67d8b037f7dab7342391fda5b
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-01-09 12:50:01 -0300

    Tipando dicionario

---
+++ estudos/typeannotations/04_dicttype.py
@@ -0,0 +1,5 @@
+# type annotation para dicionário
+
+diconario: dict[str, int] = {'a': 1}
+lista_dicionario: list[dict[str, float]] = [{'a': 1.5}, {'b': 2.5}]
+diconario_listas: dict[str, list[int]] = {'a': [1, 2, 3]}
