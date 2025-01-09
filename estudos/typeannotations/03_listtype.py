4b8c08f1c7f610b11628046d5eebeb4fd4239ba7
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-01-09 11:46:29 -0300

    type annotations para listas

---
+++ estudos/typeannotations/03_listtype.py
@@ -0,0 +1,9 @@
+# list type annotations
+
+
+lista_strings: list[str] = ['a', 'b', 'c', 'd']
+lista_tuples: list[tuple] = [(1, 2), (3, 2)]
+lista_inteiros: list[int] = [1, 2, 3, 4]
+lista_float: list[float] = [1.2, 2.3, 4.2]
+lista_de_listas: list[list[int | float]] = [[1, 3.4], [1, 3], [2.5, 1]]
+lista_dicionarios: list[dict] = [{'a': 1}, {'b': 2}]
