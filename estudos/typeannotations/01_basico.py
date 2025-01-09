1e6ba2625488674f84d36614d504fc6b1accafae
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-01-09 10:07:09 -0300

    Estudando type annotations básico

---
+++ estudos/typeannotations/01_basico.py
@@ -0,0 +1,17 @@
+# Type Annotation básico
+
+string: str = 'string'
+inteiro: int = 123
+ofloat: float = 1.2
+boleano: bool = True
+lista: list = [1, 2, 3, 4]
+dic: dict = {'a': 123, 'b': 321}
+oset: set = {1, 2, 3, 4}
+atuple: tuple = (1, 2, 3)
+
+# Necessário bibliotecas "mypy" e "flake8"
+# detecta possíveis erros
+# >mypy caminho
+
+# Apresenta erro utilizando extensões e bibliotecas mencionadas
+string = 123
