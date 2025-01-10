1a0e4809bae7b40b09cd670c50fadce61aaecca1
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-01-10 09:19:28 -0300

    type union lists

--- estudos/typeannotations/07_typeunion.py
+++ estudos/typeannotations/07_typeunion.py
@@ -4,3 +4,4 @@
 aceita_duas_tipagens = str | int   # union
 digito1: aceita_duas_tipagens = '1' or 1  # Não retorna erro
 # digito1: aceita_duas_tipagens = 1   # Não retorna erro
+tipagem_lista: list[float | str | bool] = [1.4, 's', True]
