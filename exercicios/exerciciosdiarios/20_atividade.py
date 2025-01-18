87bc8eba77b77f543c1cb66378f2e2b93c858830
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-01-18 17:55:20 -0300

    definindo estrutura

---
+++ exercicios/exerciciosdiarios/20_atividade.py
@@ -0,0 +1,20 @@
+# Dia 20: Herança
+# Crie uma classe Veiculo e faça a classe Carro herdá-la. 
+# Veiculo deve conter atributos e métodos comuns a todos os veículos.
+
+class Veiculo:
+    def __init__(self):
+        pass
+
+    def ligar(): ...
+
+    def desligar(): ...
+
+    def autonomia(): ...
+
+def main():
+    ...
+
+if __name__ == "__main__":
+    main()
+    
