f196e28db74d615193f89e96025f31b0be2b44a8
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-06 13:01:31 -0300

    aprendendo a iterar sobre páginas de um  pdf

--- estudos/modulos/16_PyPDF/07_pypdf.py
+++ estudos/modulos/16_PyPDF/07_pypdf.py
@@ -7,4 +7,17 @@
 # Link: https://pypdf2.readthedocs.io/en/3.0.0/
 # Ative seu ambiente virtual
 # pip install pypdf2
+from pathlib import Path
+from PyPDF2 import PdfReader
 
+PATH_ROOT = Path(__file__).parent
+PATH_PDF = PATH_ROOT / 'path_pdf'
+PATH_PASTE = PATH_ROOT / 'arquivo_novos'
+
+PATH_RELATORIO = PATH_PDF / 'expectativamercado.pdf'
+PATH_PASTE.mkdir(exist_ok=True)
+
+reader = PdfReader(PATH_RELATORIO)
+
+# iterável que permite ler a quantidade de páginas do PDF
+print(len(reader.pages))
